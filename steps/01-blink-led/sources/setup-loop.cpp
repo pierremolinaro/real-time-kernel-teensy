252ca39f46765128e9161ea83ffc8243c6c3d6c5
Pierre Molinaro <pierremolinaro@MacBookProPierre.local>
2018-05-28 19:39:08 +0200

    Steps 1 to 3 : using L2 led

--- steps/01-blink-led/sources/setup-loop.cpp
+++ steps/01-blink-led/sources/setup-loop.cpp
@@ -1,24 +1,24 @@
 #include "all-headers.h"
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
-// Teensy led is connected to PORTC:5 (active high)
+// Led L2 is connected to PORTD:7 (active high)
 
 void setup (void) {
-//--- Configure PTC5 as digital port (input or output)
-  PORTC_PCR (5) = PORT_PCR_MUX (1) ;
-//--- Configure PTC5 as digital output port (output level is low --> led is off)
-  GPIOC_PDDR |= (1 << 5) ;
+//--- Configure PTD7 as digital port (input or output)
+  PORTD_PCR (7) = PORT_PCR_MUX (1) ;
+//--- Configure PTD7 as digital output port (output level is low --> led is off)
+  GPIOD_PDDR |= (1 << 7) ;
 }
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 void loop (void) {
-//--- Drive PTC5 high --> led is on
-  GPIOC_PSOR = 1 << 5 ;
+//--- Drive PTD7 high --> led is on
+  GPIOD_PSOR = 1 << 7 ;
 //--- Wait...
   for (volatile uint32_t i=0 ; i< 10 * 1000 * 1000 ; i++) {}
-//--- Drive PTC5 low --> led is off
-  GPIOC_PCOR = 1 << 5 ;
+//--- Drive PTD7 low --> led is off
+  GPIOD_PCOR = 1 << 7 ;
 //--- Wait...
   for (volatile uint32_t i=0 ; i< 10 * 1000 * 1000 ; i++) {}
 }
