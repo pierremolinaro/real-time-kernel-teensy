f19604bb58ac2910c31e8951794b1316984c872e
Pierre Molinaro <pierremolinaro@MacBookProPierre.local>
2018-06-01 11:37:32 +0200

    poly -> keynotes, steps -> solutions

---
+++ solutions/01-blink-led/sources/setup-loop.cpp
@@ -0,0 +1,26 @@
+#include "all-headers.h"
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// Led L2 is connected to PORTD:7 (active high)
+
+void setup (void) {
+//--- Configure PTD7 as digital port (input or output)
+  PORTD_PCR (7) = PORT_PCR_MUX (1) ;
+//--- Configure PTD7 as digital output port (output level is low --> led is off)
+  GPIOD_PDDR |= (1 << 7) ;
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+void loop (void) {
+//--- Drive PTD7 high --> led is on
+  GPIOD_PSOR = 1 << 7 ;
+//--- Wait...
+  for (volatile uint32_t i=0 ; i< 10 * 1000 * 1000 ; i++) {}
+//--- Drive PTD7 low --> led is off
+  GPIOD_PCOR = 1 << 7 ;
+//--- Wait...
+  for (volatile uint32_t i=0 ; i< 10 * 1000 * 1000 ; i++) {}
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
---
+++ solutions/08-fault-handler--assertion/sources/setup-loop.cpp
@@ -0,0 +1,33 @@
+#include "all-headers.h"
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+void setup (USER_MODE) {
+  printString (MODE_ "Hello!") ;
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+static uint32_t gDisplayTime = 0 ;
+static uint32_t gDownCounter = 10 ;
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+void loop (USER_MODE) {
+  digitalWrite (L4_LED, !digitalRead (P4_PUSH_BUTTON)) ;
+  if (gDisplayTime <= millis ()) {
+    const uint32_t s = systick () ;
+    gotoLineColumn (MODE_ 0, 7) ;
+    printUnsigned (MODE_ millis () / gDownCounter) ;
+    gDownCounter -- ;
+    gotoLineColumn (MODE_ 1, 0) ;
+    printUnsigned (MODE_ s) ;
+    gotoLineColumn (MODE_ 2, 0) ;
+    printUnsigned (MODE_ millis ()) ;
+    gotoLineColumn (MODE_ 3, 0) ;
+    printUnsigned64 (MODE_ micros (MODE)) ;
+    gDisplayTime += 1000 ;
+  }
+}
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
