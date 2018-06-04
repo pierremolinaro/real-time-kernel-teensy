3bd95f00ec0e246da0ed1dcb238b31d6c832e898
Pierre Molinaro <pierremolinaro@MacBookProPierre.local>
2018-06-04 16:54:11 +0200

    Started step 09

--- solutions/09-critical-section/sources/setup-loop.cpp
+++ solutions/09-critical-section/sources/setup-loop.cpp
@@ -2,27 +2,51 @@
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-void setup (USER_MODE) {
-  printString (MODE_ "Hello!") ;
+static volatile uint32_t gCount1 = 0 ;
+static volatile uint32_t gCount2 = 0 ;
+static volatile uint32_t gCount3 = 0 ;
+static volatile uint32_t gCount4 = 0 ;
+static volatile bool gPerformCount = true ;
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+static void rtISR (SECTION_MODE_ const uint32_t inUptime) {
+  if (gPerformCount) {
+    gCount1 += 1 ;
+    gCount2 += 1 ;
+    gCount3 += 1 ;
+    gCount4 += 1 ;
+  }
 }
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-static uint32_t gDisplayTime = 0 ;
+MACRO_REAL_TIME_ISR (rtISR) ;
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+void setup (USER_MODE) {
+}
 
 //——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 void loop (USER_MODE) {
-  digitalWrite (L4_LED, !digitalRead (P4_PUSH_BUTTON)) ;
-  if (gDisplayTime <= millis ()) {
-    const uint32_t s = systick () ;
-    gotoLineColumn (MODE_ 1, 0) ;
-    printUnsigned (MODE_ s) ;
-    gotoLineColumn (MODE_ 2, 0) ;
-    printUnsigned (MODE_ millis ()) ;
-    gotoLineColumn (MODE_ 3, 0) ;
-    printUnsigned64 (MODE_ micros (MODE)) ;
-    gDisplayTime += 1000 ;
+  if (gPerformCount) {
+    gCount1 += 1 ;
+    gCount2 += 1 ;
+    gCount3 += 1 ;
+    gCount4 += 1 ;
+    if (5000 <= millis ()) {
+      gPerformCount = false ;
+      gotoLineColumn (MODE_ 0, 0) ;
+      printUnsigned (MODE_ gCount1) ;
+      gotoLineColumn (MODE_ 1, 0) ;
+      printUnsigned (MODE_ gCount2) ;
+      gotoLineColumn (MODE_ 2, 0) ;
+      printUnsigned (MODE_ gCount3) ;
+      gotoLineColumn (MODE_ 3, 0) ;
+      printUnsigned (MODE_ gCount4) ;
+    }
   }
 }
 
