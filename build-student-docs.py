16cbb81f3de9d90580820119ca38c63784e0c501
pierre@pcmolinaro.name <pierre@pcmolinaro.name>
2019-03-05 22:46:51 +0100

    Suite

--- build-student-docs.py
+++ build-student-docs.py
@@ -176,11 +176,11 @@ runProcess (["cp", "solutions/18-guarded-commands/sources/xtr.cpp", DOCUMENT_DIR
 compress (DOCUMENT_DIR, "18-files")
 #--- Step 19
 runProcess (["mkdir", DOCUMENT_DIR + "/19-files"])
-runProcess (["cp", "solutions/19-can-network--active-wait/sources/can/can-driver.cpp", DOCUMENT_DIR + "/19-files"])
-runProcess (["cp", "solutions/19-can-network--active-wait/sources/can/can-driver.h", DOCUMENT_DIR + "/19-files"])
-runProcess (["cp", "solutions/19-can-network--active-wait/sources/can/can-settings.cpp", DOCUMENT_DIR + "/19-files"])
-runProcess (["cp", "solutions/19-can-network--active-wait/sources/can/can-settings.h", DOCUMENT_DIR + "/19-files"])
-runProcess (["cp", "solutions/19-can-network--active-wait/sources/can/CANMessage.h", DOCUMENT_DIR + "/19-files"])
+runProcess (["cp", "solutions/19-can-network--active-send-receive/sources/can/can-driver.cpp", DOCUMENT_DIR + "/19-files"])
+runProcess (["cp", "solutions/19-can-network--active-send-receive/sources/can/can-driver.h", DOCUMENT_DIR + "/19-files"])
+runProcess (["cp", "solutions/19-can-network--active-send-receive/sources/can/can-settings.cpp", DOCUMENT_DIR + "/19-files"])
+runProcess (["cp", "solutions/19-can-network--active-send-receive/sources/can/can-settings.h", DOCUMENT_DIR + "/19-files"])
+runProcess (["cp", "solutions/19-can-network--active-send-receive/sources/can/CANMessage.h", DOCUMENT_DIR + "/19-files"])
 compress (DOCUMENT_DIR, "19-files")
 
 
